1b8ba5f3f910daaa05f7ac212b5574718b1796c6
Jeyson Buitrago <jeyson.buitrago.cepeda@interzero.de>
2025-04-13 01:46:47 +0200

    feat: Add git stash in update production script

--- shell-scripts/update-prod.sh
+++ shell-scripts/update-prod.sh
@@ -3,6 +3,13 @@
 # Go to the root of the project
 cd "$(dirname "$0")/.."
 
+# Git stash to save any uncommitted changes
+git stash
+if [ $? -ne 0 ]; then
+  echo "Failed to stash changes. Please commit or discard your changes before running this script."
+  exit 1
+fi
+
 # Fetch and pull the latest changes from the remote repository
 git fetch origin
 git pull
